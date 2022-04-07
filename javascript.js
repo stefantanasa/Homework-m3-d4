23d58b4cb435b2fd34109ae49dfc088f18c7dac7
stefantanasa <liketanasa@gmail.com>
2022-04-07 15:13:58 +0100

    UIPDATE

---
+++ javascript.js
@@ -0,0 +1,85 @@
+let books = [];
+filteredBooks = [];
+console.log("books:", books);
+const fetchData = () => {
+  fetch("https://striveschool-api.herokuapp.com/books", { method: "GET" })
+    .then((response) => response.json())
+    .then((data) => {
+      books = data;
+      console.log(data);
+      renderBooks(books);
+    });
+};
+// elements created
+const added = document.createElement("span");
+added.classList = "badge badge-primary";
+added.innerText = "Added";
+const cardItem = document.createElement("li");
+cardItem.classList.add("list-group-item");
+let countBadge = added.cloneNode(true);
+
+// render funciton
+let renderBooks = (arrayData) => {
+  // clean the target
+  rowBookList.innerHTML = "";
+  //   loop through books
+  arrayData.forEach((book) => {
+    //   create new card for card
+    let bookCard = document.createElement("div");
+    bookCard.classList.add("card");
+    bookCard.style.cssText = "width: 12rem";
+    bookCard.innerHTML = `
+        <img src=${book.img} class="card-img-top" alt="...">
+          <div class="card-body">
+          <h5 class="card-title">${book.title}</h5>
+          <p class="card-text">Category: ${book.category}</p>
+          <p class="card-text">Price: $${book.price}</p>
+          <button class="btn btn-primary" onclick="handleAdd(event)" >Add </button>
+          <button class="btn btn-primary" onclick="handleHideBook(event)">Skip</button>
+          </div>
+          `;
+
+    //   append the card
+    rowBookList.appendChild(bookCard);
+  });
+};
+
+// handleres:
+let handleAdd = (event) => {
+  //   console.log(event.target.parentNode.firstChild.innerText);
+  const toCard = event.target.closest(".card").cloneNode(true);
+  event.target.classList = "btn btn-success";
+  addedBooks.appendChild(toCard);
+  countBadge.innerText = addedBooks.children.length - 1;
+  addedBooks.prepend(countBadge);
+};
+
+let handleHideBook = (event) => {
+  event.target.closest(".card").remove();
+  countBadge.innerText = addedBooks.children.length - 1;
+  addedBooks.prepend(countBadge);
+};
+
+let handleSearch = (event) => {
+  console.log("ok", event.target.value);
+
+  if (event.target.value.length >= 3) {
+    console.log("Start filtering");
+
+    filteredBooks = books.filter((book) =>
+      book.title.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+
+    renderBooks(filteredBooks);
+  } else {
+    renderBooks(books);
+  }
+};
+// get ing elements:
+let searchBar = document.querySelector(".search-bar");
+let addedBooks = document.querySelector(".added-books");
+let rowBookList = document.querySelector(".list-books");
+
+window.onload = () => {
+  fetchData();
+};
